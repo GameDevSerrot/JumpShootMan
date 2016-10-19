cbd978dc6d123583a3cdc55a18dc538bf479d0fd
Miguel Torres <torresCodes@icloud.com>
2016-10-19 01:43:21 -0700

    JumpShootMan
    Playable

---
+++ Assets/Scripts/Destructible.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destructible : MonoBehaviour {
+    public int health;
+
+    void Start()
+    {
+        health = 2;
+    }
+
+    void Update()
+    {
+        if (health < 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+
+
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Bullet"))
+        {
+            health--;
+        }
+    }
+}
+
+
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,25 +5,78 @@ public class Player : MonoBehaviour {
 
     public int health = 3;      //the player health
 
+    bool shooting = false;
+    Animator anim;
+
     public float speed;
     public float jumpDistance;
+    public GameObject gun;
+    public GameObject bullet;
+
+    Rigidbody2D rb;
+
+    public enum PlayerState
+    {
+        running,
+
+        shooting,
+
+        jumping
+    }
+
+    public PlayerState state = PlayerState.running;
 
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-
+        //moves player right
         transform.position += transform.right * speed * Time.deltaTime;
 
-        if (Input.GetKey(KeyCode.Space))
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            transform.position += transform.up * jumpDistance * Time.deltaTime;
+            if (state == PlayerState.running)
+            {
+                //transform.position += transform.up * jumpDistance * Time.deltaTime;
+                rb.AddForce(Vector2.up * jumpDistance);
+
+                state = PlayerState.jumping;
+            }
+            else if (state == PlayerState.jumping)
+            {
+                Instantiate(bullet, gun.transform.position, Quaternion.identity);
+                shooting = true;
+                anim.SetBool("shooting", shooting);
+            }
         }
-	
+
+        switch(state)
+        {
+            case PlayerState.running:
+                shooting = false;
+                anim.SetBool("shooting", shooting);
+                break;
+            case PlayerState.shooting:
+                shooting = true;
+                anim.SetBool("shooting", shooting);
+                break;
+        }
 	}
+
+    void OnCollisionEnter2D(Collision2D collider)
+    {
+        if (collider.collider.CompareTag("Ground"))
+        {
+            state = PlayerState.running;
+            Debug.Log("derp");
+        }
+    }
 }
